4e5c4dd8975e08ee34a8240c536abe1c370ddc41
Laura Elena Mesa <mesafernandezlauraelena@gmail.com>
2024-06-05 14:10:15 -0500

    Creating the routes of the app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './pages/layout/layout.component';
+import { HomeComponent } from './pages/home/home.component';
+import { QuizComponent } from './pages/quiz/quiz.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: '',
+        component: HomeComponent
+      },
+      {
+        path: 'quiz/:name',
+        component: QuizComponent
+      }
+    ]
+  }
+];
