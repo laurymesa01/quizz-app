c8280a8976418006232e8e21a509d739ef74f6f7
Laura Elena Mesa <mesafernandezlauraelena@gmail.com>
2024-06-05 13:30:58 -0500

    Setting global colors for the app

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,17 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
+    colors: {
+      'purple': '#A729F5',
+      'dark-navy': '#313E51',
+      'navy': '#3B4D66',
+      'grey-navy': '#626C7F',
+      'light-bluish': '#ABC1E1',
+      'light-grey': '#F4F6FA',
+      'pure-white': '#FFFF',
+      'green': '#26D782',
+      'red': '#EE5454'
+    },
     extend: {},
   },
   plugins: [],
