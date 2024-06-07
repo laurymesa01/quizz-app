a0bee3c1c4658dc6c0d05f42eb0ae5c591cfed2b
Laura Elena Mesa <mesafernandezlauraelena@gmail.com>
2024-06-07 16:27:51 -0500

    Method for getting quizzes in quizz service

---
+++ src/app/services/quizz.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class QuizzService {
+
+  private http = inject(HttpClient);
+  private url = 'http://localhost:3000/quizzes';
+
+  constructor() { }
+
+  getQuizzes(){
+    return this.http.get(this.url);
+  }
+}
