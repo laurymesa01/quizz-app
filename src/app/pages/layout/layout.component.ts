e6357d0f513079ee07a285832ee704b140811ffb
Laura Elena Mesa <mesafernandezlauraelena@gmail.com>
2024-06-17 11:04:54 -0500

    Changing theme between light and dark

--- src/app/pages/layout/layout.component.ts
+++ src/app/pages/layout/layout.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 
@@ -9,6 +9,28 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
 })
-export class LayoutComponent {
+export class LayoutComponent implements OnInit{
+
+  userTheme   = localStorage.getItem("theme");
+  systemTheme = window.matchMedia("(prefers-color-scheme: dark)").matches;
+  checked: boolean = false;
+
+  ngOnInit(){
+    if(this.userTheme === 'dark' || this.systemTheme){
+      document.documentElement.classList.add('dark');
+      this.checked = true;
+    }
+  }
+
+  changeMode(){
+    if(document.documentElement.classList.contains('dark')){
+      document.documentElement.classList.remove('dark');
+      localStorage.setItem("theme", "light");
+      return;
+    }
+    document.documentElement.classList.add('dark');
+    localStorage.setItem("theme", "dark");
+    // document.documentElement.classList.toggle('dark');
+  }
 
 }
